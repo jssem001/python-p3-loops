165d5ab09dafea62fd5540172629119c86abe0d7
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-05-27 12:16:59 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,32 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    counter=10
+    while counter >0:
+        print(counter)
+        counter -=1
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    sq_list=[]
+    for i in int_list:
+        sq_list.append(i**2)
+    return sq_list
+    
 
 def fizzbuzz():
     # code goes here!
-    pass
+    counter =1
+    while counter <=100:
+        if counter % 15 == 0:
+            print("FizzBuzz")
+        elif counter % 3 == 0:
+            print("Fizz")
+        elif counter % 5 == 0:
+            print("Buzz")
+        else:
+            print(counter)
+        counter +=1
+    
